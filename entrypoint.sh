f2803d5c522fc97a31a428018ba73e66f79feb70
Martin Grünbaum <martin@itsolveonline.net>
2020-03-27 20:28:02 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,19 +22,13 @@ else
   asset_id=`curl -H "Accept: application/vnd.github.v3.raw" -s https://$GITHUBAPI/repos/$REPO/releases | jq "$parser"`
 fi;
 
-echo "Asset ID: $asset_id"
 if [ "$asset_id" = "null" ]; then
   echo "ERROR: Version $VERSION was not found! 1>&2"
   exit 1
 fi;
 
 if [ ! -z "$TOKEN" ]; then
-  #wget --auth-no-challenge --header='Accept:application/octet-stream' https://$TOKEN:@$GITHUBAPI/repos/$REPO/releases/assets/$asset_id -O $FILE
   curl -sLJO -H "Authorization: token ${TOKEN}" -H 'Accept:application/octet-stream' https://$GITHUBAPI/repos/$REPO/releases/assets/$asset_id
-  echo "Done downloading $FILE"
-  cat $FILE
 else
-  wget --header='Accept:application/octet-stream' https://$GITHUBAPI/repos/$REPO/releases/assets/$asset_id -O $FILE
-  echo "Done downloading $FILE"
-  cat $FILE
+  curl -sLJO -H "Accept:application/octet-stream" https://$GITHUBAPI/repos/$REPO/releases/assets/$asset_id
 fi;
